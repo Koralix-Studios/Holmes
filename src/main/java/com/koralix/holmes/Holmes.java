0daad4403961b929a609a1b866aa4db5b350e770
JohanVonElectrum <joanestradium@gmail.com>
2023-04-27 07:54:39 +0200

    Optimize imports

--- src/main/java/com/koralix/holmes/Holmes.java
+++ src/main/java/com/koralix/holmes/Holmes.java
@@ -1,11 +1,9 @@
 package com.koralix.holmes;
 
-import com.koralix.holmes.db.matrix.Matrix;
 import com.koralix.holmes.db.matrix.MatrixStorage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
 import java.io.IOException;
 
 public class Holmes {
